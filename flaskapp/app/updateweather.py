f006e2ec392098f64c9d6288a4b945b3038cd587
Eunice Kim <ekimed@gmail.com>
2013-12-17 02:28:03 -0800

    Adding a new file

---
+++ flaskapp/app/updateweather.py
@@ -0,0 +1,15 @@
+import urllib2
+import json
+import re
+
+
+coordinate_dict = {'Bishop':'37.3291,-118.5771'}
+for key, value in coordinate_dict.iteritems():
+    f = urllib2.urlopen('http://api.wunderground.com/api/fa93bf8af6931895/forecast10day/geolookup/q/'+value+'.json')
+    json_string = f.read()
+    parsed_json = json.loads(json_string)
+    filename = key+'.json'
+    with open(filename, "wb") as outfile:
+        json.dump(parsed_json, outfile, indent=6)
+
+
