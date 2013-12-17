e25f40bfcf71efdc3cc00be4eb3764da0f7074fd
Eunice Kim <ekimed@gmail.com>
2013-12-17 03:24:07 -0800

    Change updateweather.py

--- BishopWeather/app/updateweather.py
+++ BishopWeather/app/updateweather.py
@@ -4,7 +4,7 @@ import json
 
 coordinate_dict = {'Bishop':'37.3291,-118.5771'}
 for key, value in coordinate_dict.iteritems():
-    #insert personal API key from Wunderground API
+    #apikey--insert personal API key from Wunderground API
     f = urllib2.urlopen('http://api.wunderground.com/api/apikey/forecast10day/geolookup/q/'+value+'.json')
     json_string = f.read()
     parsed_json = json.loads(json_string)
